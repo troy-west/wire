c729ce064b32388bbfdd88446ddc4c2cbb357807
Derek Troy-West <derek@troywest.com>
2018-07-10 15:44:44 +1000

    changelog, readme, tidy-up

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to topiary
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++ project.clj
@@ -5,7 +5,26 @@
   
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
-  
-  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
+
+  :plugins [[lein-cljfmt "0.5.7" :exclusions [org.clojure/clojure]]
+            [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
+            [lein-kibit "0.1.6" :exclusions [org.clojure/clojure org.clojure/tools.reader]]]
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [rhizome "0.2.7"]])
+                 [rhizome "0.2.7"]]
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+
+  :aliases {"smoke" ["do" ["clean"] ["check"] ["test"] ["kibit"] ["cljfmt" "check"] ["eastwood"]]}
+
+  :pedantic? :abort)
