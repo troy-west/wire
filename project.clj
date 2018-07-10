2300ec2ea4dd4b3d7c7fd03d270e0097930aad1e
Derek Troy-West <derek@troywest.com>
2018-07-10 15:45:05 +1000

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.troy-west/wire "0.1.0-SNAPSHOT"
+(defproject com.troy-west/wire "0.1.0"
   :description "A small Clojure library for explicitly wiring together functions into declarative computation graphs."
   
   :url "https://github.com/troy-west/wire.git"
